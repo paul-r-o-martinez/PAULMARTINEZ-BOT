cf0c3010af7c3b26dca1ad19346d33ca9af446cf
paul-r-o-martinez <paul.r.o.martinez@accenture.com>
2018-11-28 19:50:47 +0800

    Add Web App

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=paulmartinez-bot-Web-Deploy.pubxml -p:Password=3Wrkqpqz9M6FAWjCqZQTAgZyvKZrBqFk9cGnkCiySjoeBpxpm8WkbL2w4snj
+
